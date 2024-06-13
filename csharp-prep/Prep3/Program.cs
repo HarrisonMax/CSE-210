89c7c359aab362d3f590c3ffec05349901f76b04
Harrisonmax <Harrisonmax@byui.edu>
2024-06-13 14:33:44 -0600

    Did prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,57 @@
 using System;
+using System.Drawing;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+  {
+        //  string response = "yes";
+        // // string colors = "orange";
+
+        //  while (response == "yes")
+        //  {
+        //     Console.Write("Do you want to continue? ");
+        //     response = Console.ReadLine();
+        // }
+        // do
+        // {
+        //     Console.Write("Do you want to continue?");
+        //     response = Console.ReadLine();
+        // }while (response == "yes");
+
+        // for (int i = 0; i < 10; i++)
+        // {
+        //     Console.WriteLine(i);
+        // }
+        
+        // foreach (string color in colors)
+        // {
+        //     Console.WriteLine(color);
+        // }
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 11);
+        int guessMagicNumber = -1;
+
+        while (guessMagicNumber != magicNumber)
+        {
+            Console.WriteLine("What is your guess? ");
+            guessMagicNumber = int.Parse(Console.ReadLine());
+            
+            if (guessMagicNumber < magicNumber)
+            {
+                Console.Write("your guess is too Low ");
+            }
+            else if (guessMagicNumber > magicNumber)
+            {
+                Console.Write("Your guess is too High");
+            }
+            else
+            {
+                Console.Write("You Guessed it!");
+            }
+        }
+
+        
+  }
 }
