be23cf41077243742f7b962a9cb1b8d727d7ab94
Harrisonmax <Harrisonmax@byui.edu>
2024-06-17 14:43:19 -0600

    updated

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,13 @@
 using System;
+using System.Data;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        
+        Circle myCircle = new Circle(10);
+        Console.WriteLine($"{myCircle.GetArea()}");
     }
 }
---
+++ sandbox/Sandbox/circle.cs
@@ -0,0 +1,12 @@
+class Circle
+{
+    private double _radius;
+    public Circle(double radius = 0.0)
+    {
+        _radius = radius;
+    }
+    public double GetArea()
+    {
+        return Math.PI * _radius * _radius;
+    }
+}
