0861a8c027280dab1aca741db2aba5a51b420d6d
Harrisonmax <Harrisonmax@byui.edu>
2024-06-20 14:50:30 -0600

    fixed

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -6,4 +6,11 @@ public class JournalEntry
     public string Prompt { get; set; }
     public string Response { get; set; }
     public DateTime Date { get; set; }
+
+    // public override string ToString()
+    // {
+    //    string outputString = "";
+    //    outputString 
+    //     return base.ToString()
+    // }
 }
