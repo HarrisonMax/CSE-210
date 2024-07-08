e2a1262d942eac4d690b7b1524b79f3c4501751c
Harrisonmax <Harrisonmax@byui.edu>
2024-07-08 13:02:22 -0600

    followed example

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+   {
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Yellow", 6);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("Blue", 8, 10);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("Red", 9);
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
+}
 }
