ecdb1bedaa9c5ffd14a72b9408cd066225c9e304
Harrisonmax <Harrisonmax@byui.edu>
2024-07-09 14:15:48 -0600

    still working

--- sandbox/Sandbox/sandbox2.cs
+++ sandbox/Sandbox/sandbox2.cs
@@ -8,12 +8,22 @@ class Program
     {
         Console. WriteLine("Bonjour Tout Le Monde");
         Employee employee = new Employee("Bob", 40, 23.77);
-        Console.WriteLine($"{employee.GetName()} Is due: {employee.GetPay()}");
+    //    Console.WriteLine($"{employee.GetName()} Is due: {employee.GetPay()}");
 
         ConstructionWorker constructionWorker = new ConstructionWorker("Betty", 40, 35.88);
-        Console.WriteLine($"{constructionWorker.GetName()} is due: {constructionWorker.GetPay()}");
+      //  Console.WriteLine($"{constructionWorker.GetName()} is due: {constructionWorker.GetPay()}");
 
         Doctor doctor = new Doctor("Belinda", 240000.99);
-        Console.WriteLine($"{doctor.GetName()} is due {doctor.GetPay()}");
+        // Console.WriteLine($"{doctor.GetName()} is due {doctor.GetPay()}");
+
+        List<Employee> employees = new List<Employee>();
+        employees.Add(employee);
+        employees.Add(constructionWorker);
+        employees.Add(doctor);
+
+        foreach (Employee e in employees)
+        {
+            Console.WriteLine($"{e.GetName()} is due {e.GetPay()}");
+        }
     }
 }
